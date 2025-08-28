27819e01e6e6a2ea4a3bec6f69bddf0b12f60e06
0213digital <0213digital@gmail.com>
2025-08-28 18:41:06 +0100

    1

--- src/main.jsx
+++ src/main.jsx
@@ -2,24 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-import { LanguageProvider } from './contexts/LanguageContext';
+// REMOVED: import { LanguageProvider } from './contexts/LanguageContext';
 import { SessionProvider } from './contexts/SessionContext';
 import { ProfileProvider } from './contexts/ProfileContext';
 import { AuthProvider } from './contexts/AuthContext.jsx';
 import './index.css';
 
+// ADDED: Import the i18next configuration to initialize it
+import './i18n'; 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <LanguageProvider>
-      <BrowserRouter>
-        <SessionProvider>
-          <ProfileProvider>
-            <AuthProvider>
-              <App />
-            </AuthProvider>
-          </ProfileProvider>
-        </SessionProvider>
-      </BrowserRouter>
-    </LanguageProvider>
+    {/* REMOVED: <LanguageProvider> wrapper */}
+    <BrowserRouter>
+      <SessionProvider>
+        <ProfileProvider>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </ProfileProvider>
+      </SessionProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
